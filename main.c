6bc72e9b8975d85b224d8dadf01df1cb6c35e6ca
TerrificTable <omegagaming189@gmail.com>
2023-05-22 07:34:14 +0200

    fixed ascii?

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+#include <locale.h>
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #include "common.h"
 
@@ -8,6 +13,7 @@
 
 char* l1; char* l2; char* l3; char* l4; char* l5;
 char* osName;
+char* ip;
 
 int mem_total;
 float mem_used, mem_perc;
@@ -26,6 +32,12 @@ float cpu_usage();
 
 
 int main(unused int argc, unused char *argv[]) {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#else
+    setlocale(LC_CTYPE, "");
+#endif
+
     ascii(&l1, &l2, &l3, &l4, &l5);
     memory_usage(&mem_total, &mem_used, &mem_perc);
     if (mem_total == -1 || mem_used == -1 || mem_perc == -1) {
@@ -40,11 +52,14 @@ int main(unused int argc, unused char *argv[]) {
     }
 
     os(&osName);
+    network(&ip);
+
+    setlocale(LC_CTYPE, "");
 
     printf(" %s  cpu  %.1f%%\n", l1, cpu_usage());
     printf(" %s  mem  %.2f/%d gb (%.1f%%)\n", l2, mem_used, mem_total, mem_perc);
-    printf(" %s  dsk  %.2f/%d gb (%.1f%%)\n", l3, disk_used, disk_total, disk_perc);
-    printf(" %s  ip   todo\n", l4);
+    printf(" %s  dsk  %.1f/%d gb (%.1f%%)\n", l3, disk_used, disk_total, disk_perc);
+    printf(" %s  ip   %s\n", l4, ip);
     printf(" %s  os   %s\n", l5, osName);
 
     return 0;
